5167029e9bd48cb94f35509628535f56b5fb2403
Javi Roman <javiroman@redoop.org>
2014-02-02 10:46:54 +0100

    more log4j

--- buildoop/conf/log4jconfig.groovy
+++ buildoop/conf/log4jconfig.groovy
@@ -1,9 +1,13 @@
 log4j {
-    appender.stdout = "org.apache.log4j.ConsoleAppender"
-    appender."stdout.layout"="org.apache.log4j.PatternLayout"
+    // log file appender with file rotation.
     appender.scrlog = "org.apache.log4j.FileAppender"
-    appender."scrlog.layout"="org.apache.log4j.TTCCLayout"
+    appender.scrlog = "org.apache.log4j.RollingFileAppender"
+    appender."scrlog.MaxFileSize"="1MB"
+    appender.'srclog.MaxBackupIndex'="1"
+    appender."scrlog.layout"="org.apache.log4j.PatternLayout"
+    appender."scrlog.layout.ConversionPattern"="%d %5p %r %c{1}: %m%n"
     appender."scrlog.file"="build/log/buildoop.log"
  
-    rootLogger="debug,scrlog,stdout"
+    // root logger level and appender attached.
+    rootLogger="trace,scrlog"
 }
