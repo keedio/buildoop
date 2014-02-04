e183afafe0438836327bff301516f8588fe9fa3e
Javi Roman <jroman.espinar@gmail.com>
2014-02-04 09:48:28 +0100

    automatic version annotation

--- scripts/set-version-file.sh
+++ scripts/set-version-file.sh
@@ -1,1 +1,16 @@
-echo "hola"
+#!/bin/sh
+
+if [ -z "$BDROOT" ]; then
+	echo "BDROOT is not set"
+	return 1
+fi
+
+conffile=${BDROOT}/buildoop/conf/buildoop.conf
+string="Buildoop v0.0.1-alpha"
+hash=$(git rev-parse --short HEAD)
+
+version=$string-$hash 
+echo $version > ${BDROOT}/VERSION
+
+sed -i -r "s/buildoop.version.*/buildoop.version=\"$version\"/g" ${conffile}
+
