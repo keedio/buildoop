b89d6999aded7244c0cd3da162b78e4abefeaf40
Javi Roman <jroman.espinar@gmail.com>
2014-02-14 23:16:32 +0100

    http server tested with repodata

--- buildoop/lib/EmbeddedHTTPServer.groovy
+++ buildoop/lib/EmbeddedHTTPServer.groovy
@@ -1,10 +1,12 @@
 import com.sun.net.httpserver.*
 import groovy.json.*
 
-println "Create server port 8000"
-HttpServer server = HttpServer.create(new InetSocketAddress(8000),0);
-server.createContext("/test", new MyHandler(server:server));
-server.createContext("/kill", new KillHandler(server:server));
+HTTP_SERVER_PORT=8080
+println "Create server port " + HTTP_SERVER_PORT
+server = HttpServer.create(new InetSocketAddress(HTTP_SERVER_PORT),0);
+
+server.createContext("/", new MyHandler(server:server));
+//server.createContext("/kill", new KillHandler(server:server));
 server.setExecutor(null); // creates a default executor
 println "Starting server"
 server.start();
@@ -14,17 +16,25 @@ class MyHandler implements HttpHandler {
     def server
 
     public void handle(HttpExchange exchange) throws IOException {
-        def requestMethod = exchange.requestMethod
-        exchange.responseHeaders['Content-Type'] = 'application/json'
-        def builder = new JsonBuilder()
-        builder {
-            success true
-            method requestMethod
-        }
-        def response = builder.toString()
-        exchange.sendResponseHeaders(200, response.length());
-        exchange.getResponseBody().write(response.bytes);
-        exchange.close();
+	println "getRequestMethod:"
+	println exchange.getRequestMethod() 
+	println "getRequestHeaders:"
+	println exchange.getRequestHeaders()
+	println "getRequestURI:"
+	def fileName = exchange.getRequestURI()
+	println fileName 
+
+	def file = new File("." + fileName)
+      	def bytearray  = new byte [(int)file.length()]
+      	def fis = new FileInputStream(file)
+      	def bis = new BufferedInputStream(fis);
+      	bis.read(bytearray, 0, bytearray.length);
+
+      	// ok, we are ready to send the response.
+      	exchange.sendResponseHeaders(200, file.length());
+      	def os = exchange.getResponseBody();
+      	os.write(bytearray,0,bytearray.length);
+      	os.close()
     }
 }
 
