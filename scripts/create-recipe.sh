d39c462da27dfbc778f877998c4fa1719184845f
Javi Roman <jroman.espinar@gmail.com>
2014-06-01 16:59:03 +0200

    fixed create-recipe

--- scripts/create-recipe.sh
+++ scripts/create-recipe.sh
@@ -79,6 +79,12 @@ basedir=${BDROOT}/recipes/${RECIPE_NAME}/${RECIPE_NAME}-${RECIPE_VERSION}_${RECI
 mkdir -p ${basedir}/rpm/sources
 mkdir -p ${basedir}/rpm/specs
 
+cat <<! > ${BDROOT}/recipes/${RECIPE_NAME}/ChangeLog
+DD-MM-YYYY User Committer <usercommiter@domain.org>
+
+	* Initial commit.
+!
+
 cat <<! > ${BDROOT}/recipes/${RECIPE_NAME}/${RECIPE_NAME}-${RECIPE_VERSION}_${RECIPE_REVISION}.bd
 {
 	"do_info": {
